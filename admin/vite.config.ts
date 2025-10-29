b9d49005c17d89f8055d38be546f26c6c15d320e
CaoDat1603 <caotiendat1604@gmail.com>
2025-10-30 01:58:08 +0700

    chore(config): update Vite server configuration to change default port

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,     // 👈 Đổi thành cổng bạn muốn (mặc định là 5173)
+    //host: true,     // Cho phép truy cập qua IP mạng LAN
+    //open: true      // Tự động mở trình duyệt khi chạy `npm run dev`
+  }
 })
